c9d76e47ab8ee1c89140e7c5744f4af31bbed669
manoj puthalapattu <58524654+manojputhalapattu@users.noreply.github.com>
2020-07-24 21:57:00 +0530

    event-updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,19 @@ import React, { useState } from "react";
 
 function App() {
   const[changedevents,setchangedevent]=useState("hello")
+  const[clicked,setclicked]=useState("hello")
   function changedevent(event){
 setchangedevent(event.target.value)
+  }
+  function clickedevent(){
+setclicked(changedevents)
   }
   
   return (
     <div className="container">
-      <h1>{changedevents} </h1>
-      <input onChange={changedevent} type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>{clicked} </h1>
+      <input onChange={changedevent} name="names" type="text" placeholder="What's your name?" />
+      <button onClick={clickedevent}>Submit</button>
     </div>
   );
 }
